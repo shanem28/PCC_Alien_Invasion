019d7d4a4a1938951d5d693c51f2a31eb03b89f4
Shane Meers <shanem28@gmail.com>
2023-01-30 16:51:28 -0700

    button color

--- button.py
+++ button.py
@@ -1,5 +1,6 @@
 import pygame.font
 
+
 class Button:
 
     def __init__(self, ai_game, msg):
@@ -9,7 +10,7 @@ class Button:
 
         # Set the dimensions and properties of the button
         self.width, self.height = 200, 50
-        self.button_color = (0, 255, 0)
+        self.button_color = (0, 135, 0)
         self.text_color = (255, 255, 255)
         self.font = pygame.font.SysFont(None, 48)
 
